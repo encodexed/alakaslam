07ca153a5a589ab448d1fb5ca424e1bbcc7c371f
Robbie Gollan <robbiegollan@gmail.com>
2023-05-01 17:37:59 +1000

    Small UX change

--- src/components/Options/CombinationsOptions.js
+++ src/components/Options/CombinationsOptions.js
@@ -5,7 +5,7 @@ export default function CombinationsOptions(props) {
 		<div className='flex h-10 py-2 border-b-2 border-black'>
 			<div className='flex-1 pl-2'>
 				Filter:
-				<input type='text' className='h-6 p-1 ml-1 border rounded-lg' placeholder='Columbine'/>
+				<input type='text' className='h-6 p-1 ml-1 border rounded-lg' placeholder='eg. Columbine'/>
 			</div>
 			<div className='flex-1 pr-2 text-right'>
             <Toggle toggleStrictMode={props.toggleStrictMode} strictMode={props.strictMode} /> Strict Mode
