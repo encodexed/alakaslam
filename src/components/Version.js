192e3826d4ec0851a08d6625549d6858f571cf71
Robbie Gollan <robbiegollan@gmail.com>
2023-07-08 16:26:36 +1000

    update version status

--- src/components/Version.js
+++ src/components/Version.js
@@ -5,6 +5,6 @@ export default function Version(props) {
    }
 
    return (
-      <p className='p-0 m-0 text-xs text-center'>Version: Update 37 Scribes of Fate (4/23) - Need <button className='text-blue-500 underline' onClick={openInfo}>help?</button></p>
+      <p className='p-0 m-0 text-xs text-center'>Version: Update 38 Necrom (7/23) - Need <button className='text-blue-500 underline' onClick={openInfo}>help?</button></p>
    )
 }
